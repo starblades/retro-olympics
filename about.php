b00a39cb3a303b3db80d6984fe4565f1092241bc
moooon/starblades <98715022+starblades@users.noreply.github.com>
2022-11-15 20:13:59 -0600

    Header 1.0, Body and About Pages
    Includes Header 1.0, Body and About pages being called in a php file.

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <?php echo file_get_contents("htmllinks/header.html"); ?>
-<?php echo file_get_contents("htmllinks/body.html"); ?>
+<?php echo file_get_contents("htmllinks/about.html"); ?>
 <p>Made by starblades <?php echo date("Y"); ?></p>
